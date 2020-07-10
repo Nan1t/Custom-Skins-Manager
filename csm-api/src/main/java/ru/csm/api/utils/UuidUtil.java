f15a3b2d51ca72a17937d5bb80ff5021d1dd6fe8
Nanit <nanitmix@gmail.com>
2020-07-10 18:54:37 +0300

    Added license

--- csm-api/src/main/java/ru/csm/api/utils/UuidUtil.java
+++ csm-api/src/main/java/ru/csm/api/utils/UuidUtil.java
@@ -1,3 +1,21 @@
+/*
+ * Custom Skins Manager
+ * Copyright (C) 2020  Nanit
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package ru.csm.api.utils;
 
 import java.util.UUID;
@@ -5,10 +23,7 @@ import java.util.UUID;
 public class UuidUtil {
 
     public static UUID getUUID(String str){
-        if(str.contains("-")){
-            return UUID.fromString(str);
-        }
-
+        if(str.contains("-")) return UUID.fromString(str);
         return UUID.fromString(str.replaceFirst("(\\p{XDigit}{8})(\\p{XDigit}{4})(\\p{XDigit}{4})(\\p{XDigit}{4})(\\p{XDigit}+)", "$1-$2-$3-$4-$5"));
     }
 
