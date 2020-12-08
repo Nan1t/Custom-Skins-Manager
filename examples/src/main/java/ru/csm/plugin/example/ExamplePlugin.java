88fa21eea5fe70bbd8096b253f8ce5bef11b35ac
Nan1t <nanitmix@gmail.com>
2020-12-08 21:23:14 +0200

    Update test plugin

--- examples/src/main/java/ru/csm/plugin/example/ExamplePlugin.java
+++ examples/src/main/java/ru/csm/plugin/example/ExamplePlugin.java
@@ -21,11 +21,14 @@ package ru.csm.plugin.example;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 import ru.csm.api.player.Skin;
 import ru.csm.api.services.SkinsAPI;
+import ru.csm.bukkit.event.SkinChangeEvent;
 
-public class ExamplePlugin extends JavaPlugin {
+public class ExamplePlugin extends JavaPlugin implements Listener {
 
     private static final String TEXTURE = "ewogICJ0aW1lc3RhbXAiIDogMTU5NTM2MjIxOTE0MSwKICAicHJvZmlsZUlkIiA6ICJiMGQ3MzJmZTAwZjc0MDdlOWU3Zjc0NjMwMWNkOThjYSIsCiAgInByb2ZpbGVOYW1lIiA6ICJPUHBscyIsCiAgInNpZ25hdHVyZVJlcXVpcmVkIiA6IHRydWUsCiAgInRleHR1cmVzIiA6IHsKICAgICJTS0lOIiA6IHsKICAgICAgInVybCIgOiAiaHR0cDovL3RleHR1cmVzLm1pbmVjcmFmdC5uZXQvdGV4dHVyZS81YThjNTUzOGI3NjI5ZGJlMjVkZmY1ZjJiMjkyY2E2ODE3MDJjNmUwOTY3ODVjZDY2YjVhYTc5MjM5NDFkOTI1IiwKICAgICAgIm1ldGFkYXRhIiA6IHsKICAgICAgICAibW9kZWwiIDogInNsaW0iCiAgICAgIH0KICAgIH0KICB9Cn0=";
     private static final String SIGNATURE = "HoT9lIHhyQd/C4/CmrBIKIw9w8OI3Fk4kQ/HLyGRaPRgLnY5UuOaJe5+s9XRanjTOgnXmQPIXKI1Xx1At2/pcft7drvxLClqY8gkamIummt030vsff/m5ub0P3BYVOivo1fmxOXYi4U3u+ydYvaUk1kagyY212U6ufobR3Mu1ITmeSVO1E9r/HEOnu0zGwWL76zi2c6i7V+5fmxWdv9OyaDGscexyT57cqgrR5NEZOIeS6puOxKmXx0gXFcDj5y8S9bQNgZo1ZAuSjSOMhs9oy+Jh3p+vLfwTF9AOSgLNc3aly1nAxIQrrAGqivnKdK0FHUuSDOLP6vMJ7GgprtHvvKYOOYCY5lrPDoR9vrNoIu2ibNlq8oNii8SVu9nmMyuudc8m44xkWvJ9/HdRwaGtsPQP8A4DW5KhEmBEywPpYZxH/YlFCPqkoVj+3ije/Vss3X7j9CenZ2h4XLZgnmF12kKGEg5tyhvrd7mRoYnaWOUuvDYgyfYsVdFaPfMkI5jm3KdEaAAMvPE9sFmAbMUFMPecQBGnciYT95lONugglEyUT0wIF5tCpJE/m+A2idlorlJ3MvXRno8WY6tcyZYnAFooDY+y6yDydNw102BA5qqzLupMXl9n0A39VztB4wA9OLad+sx4tOG6qWVcCSJZOXJmjOI7E8fEXSq89ELzis=";
@@ -35,6 +38,8 @@ public class ExamplePlugin extends JavaPlugin {
     @Override
     public void onEnable() {
         api = getServer().getServicesManager().getRegistration(SkinsAPI.class).getProvider();
+
+        getServer().getPluginManager().registerEvents(this, this);
     }
 
     @Override
@@ -45,5 +50,10 @@ public class ExamplePlugin extends JavaPlugin {
         }
 
         return true;
+    }
+
+    @EventHandler
+    public void onSkinChange(SkinChangeEvent event){
+        event.setNewSkin(new Skin(TEXTURE, SIGNATURE));
     }
 }
